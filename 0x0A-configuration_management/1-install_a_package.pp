ca9cde5582d8d83cb196fef3fcdaf7e378d0fdda
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-11-27 02:49:38 -0800

    Configuration

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,10 @@
 #This install flask from pip3 using puppet
+
 package { 'python3-pip':
-  ensure => 'installed',
+  ensure => installed,
 }
 
-package { 'flask':
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
   require  => Package['python3-pip'],
