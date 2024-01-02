9b490438666f0cd668335ad037782502299a34a4
Grandkojo <essenernest.kojoowusu@gmail.com>
2024-01-02 14:05:37 -0800

    Export to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""This is a call to a REST API"""
+
+import requests
+import sys
+
+
+def return_employee_info(emp_id):
+    """This method returns the todo  progress of the employer"""
+
+    user_url = f"https://jsonplaceholder.typicode.com/users/{emp_id}"
+    todo_url = f"https://jsonplaceholder.typicode.com/user/{emp_id}/todos"
+
+    req_user = requests.get(user_url)
+    req_todo = requests.get(todo_url)
+
+    if req_user.status_code != 200 or req_todo.status_code != 200:
+        print("Error: Can't fetch any data from API")
+        return
+    user_data = req_user.json()
+    todo_data = req_todo.json()
+
+    total_task = len(todo_data)
+    task_done = len([task for task in todo_data if task.get("completed")])
+    with open(f"{emp_id}.csv", "w") as files:
+        for task in todo_data:
+            files.write(f'"{emp_id}, {user_data.get("username")}", '
+                        f'"{task.get("completed")}", {task.get("title")}')
+
+
+#   if task.get('completed')])
+#   print(f"Employee {user_data.get('name')} is done "
+#        f"with tasks({task_done}/{total_task}):")
+
+#    for tasks in todo_data:
+#       if tasks.get('completed'):
+#          print(f"\t " + tasks.get('title'))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} <employee_id>")
+        sys.exit(1)
+    return_employee_info(sys.argv[1])
